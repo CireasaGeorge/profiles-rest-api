83877e453c689ca1a2959da52c5dfe9a51ea80b1
Cireasa George <george.cireasa@ivfuture.uk>
2021-10-26 15:00:58 +0300

    Added post Request

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,8 +1,16 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from profiles_api import serializers
+
+
+
 
 class HelloApiView(APIView):
     """Test API View"""
+
+    serializer_class = serializers.HelloSerializer
+
     def get(self,request,formt=None):
         """returns a list of APIView features"""
         an_apiview = [
@@ -12,6 +20,19 @@ class HelloApiView(APIView):
         'Is mapped manyally to URLs'
         ]
 
-        return Response({'message':"Hello",
-        'an_apiview':an_apiview}
-        )
+        return Response({'message':"Hello",'an_apiview':an_apiview})
+
+    def post(self,request):
+        """Create a hello message with our name"""
+        serializer=self.serializer_class(data=request.data) # data takes value of requested data
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name') # retrieve the name from the vlidated data
+            message = f'Hello {name}'
+            return Response({'message':message})  # returns a message to the  http 200
+        else:
+            return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST)
+
+             # return a message to the http 400   bad request.
